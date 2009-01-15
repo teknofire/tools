b439cb5de5dcc59f26c1d4152feee1cc7e7e2840
Will Fisher <will@gina.alaska.edu>
2009-01-15 11:05:46 -0900

    split dgs class into it's own file to require, created a monitor script

--- ruby/go/dgs.rb
+++ ruby/go/dgs.rb
@@ -43,21 +43,3 @@ class DgsUser
     @games.reject { |id,game| game[:new] == false }
   end
 end
-
-if __FILE__ == $0
-  u = DgsUser.new(ARGV.shift)
-  loop do 
-    u.check
-    u.parse
-
-    unless u.new_games.empty?
-      puts "----------------- #{Time.now} ---------------"
-      u.new_games.each do |id, game|
-        `mumbles-send "New DGS Activity" "#{game[:date]} - #{game[:opponent]}"`
-        puts game[:opponent] 
-      end
-    end
-
-    sleep 30
-  end
-end
